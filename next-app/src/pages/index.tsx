2601c68b970fdda73f8dc1a806a7015576abb5b9
Brlaney <Brlaney@outlook.com>
2024-07-17 19:16:19 -0500

    s

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -4,11 +4,7 @@ import styles from '@/styles/pages/Home.module.scss';
 export default function Home() {
   return (
     <div className={styles.homepage}>
-
-      <div className={styles.title}>
-        Django and next js
-      </div>
-      
+      <div className={styles.title}>Django and next js</div>
       <div className={styles.content}>
         <div className={styles.card}>
 
